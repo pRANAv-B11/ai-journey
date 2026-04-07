42202b5ec88441e4f72c1104c7889a7b6d2613e6
pRANAv-B11 <pranavicar2464@gmail.com>
2026-04-07 23:42:47 +0530

    Day 1: Python basics + f-strings + function

---
+++ day1_basics.py
@@ -0,0 +1,15 @@
+# Day 1 - Basics
+
+name = 'pranav'
+name = name.capitalize()
+
+age = 22
+
+print(f'Hello I am {name}.')
+print(f'I am {age} years old.')
+
+#function
+def greet(user):
+	return f"Welcome to AI journey!, {user}!"
+
+print(greet(name))
